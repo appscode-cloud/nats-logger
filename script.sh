5f2d434499909cce4c64ac2482ca6379c730aae5
Tamal Saha <tamal@appscode.com>
2022-12-23 20:38:43 +0600

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- script.sh
+++ script.sh
@@ -2,12 +2,18 @@
 
 # <UDF name="my_var" label="My Variable" />
 
-set -x
+# set -x
 
 MY_VAR=${1:-xyz}
 
 exec >/tmp/stackscript.log 2>&1
 echo 'running demo script'
 echo ${MY_VAR}
+
+while :
+do
+	echo "Press [CTRL+C] to stop.."
+	sleep 1
+done
 
 touch /tmp/success.txt
