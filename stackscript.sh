fcfb67ca71c003e51cd1ab5d53fb144283a883ae
Tamal Saha <tamal@appscode.com>
2022-12-27 02:24:09 +0600

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- stackscript.sh
+++ stackscript.sh
@@ -10,10 +10,14 @@ set -xeou pipefail
 exec >/root/stackscript.log 2>&1
 
 # http://redsymbol.net/articles/bash-exit-traps/
+# https://unix.stackexchange.com/a/308209
 function finish {
-    echo $? > /root/result.txt
+    result=$?
+    [ ! -f /root/result.txt ] && echo $result > /root/result.txt
 }
 trap finish EXIT
+
+pwd
 
 curl -fsSLO https://github.com/tamalsaha/ssh-exec-demo/raw/master/producer/producer
 chmod +x ./producer
@@ -21,3 +25,9 @@ SHIPPER_FILE=/root/stackscript.log ./producer &
 
 echo 'running demo script'
 echo ${MY_VAR}
+
+for i in 1 2 3 4 5
+do
+    echo "Press [CTRL+C] to stop.."
+    sleep 1
+done
