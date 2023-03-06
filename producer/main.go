5ff898143dada4dac94467c7773e58dd70c2b6b8
Masudur Rahman <masud@appscode.com>
2023-03-06 18:03:20 +0600

    Publish logs as natjobs response
    Signed-off-by: Masudur Rahman <masud@appscode.com>

--- producer/main.go
+++ producer/main.go
@@ -1,30 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"os"
 	"time"
 
 	"github.com/hpcloud/tail"
 	"github.com/nats-io/nats.go"
+	"github.com/rs/xid"
 
 	"github.com/tamalsaha/ssh-exec-demo/internal/util"
 )
-
-func publishFile(source string, subject string, nc *nats.Conn) error {
-	t, err := tail.TailFile(source, tail.Config{Follow: true})
-	if err != nil {
-		return err
-	}
-
-	log.Printf("Publishing lines from %s to %s", source, subject)
-
-	for line := range t.Lines {
-		nc.Publish(subject, []byte(line.Text))
-	}
-
-	return nil
-}
 
 func main() {
 	source := os.Getenv("SHIPPER_FILE")
@@ -59,13 +46,65 @@ func main() {
 		log.Fatalf("Could not connect to NATS: %s\n", err)
 	}
 
+	id := xid.New().String()
+	title, ok := os.LookupEnv("SHIPPER_TITLE")
+	if !ok {
+		title = "Create Linode Instance"
+	}
+
+	msg := newResponse(TaskStatusStarted, id, title, "Creating Linode Instance")
+	if err = nc.Publish(subject, msg); err != nil {
+		log.Printf("Could not publish response")
+	}
+
 	for {
 		// fmt.Sprintf("%s.p%d.%s", subject, partition, name)
-		err = publishFile(source, subject, nc)
+		err = publishFile(source, subject, nc, id, title)
 		if err != nil {
 			log.Printf("Could not publish file: %s", err)
 		}
 
 		time.Sleep(500 * time.Millisecond)
 	}
+}
+
+func publishFile(source string, subject string, nc *nats.Conn, id, title string) error {
+	t, err := tail.TailFile(source, tail.Config{Follow: true})
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Publishing lines from %s to %s", source, subject)
+
+	for line := range t.Lines {
+		msg := newResponse(TaskStatusRunning, id, title, line.Text)
+		nc.Publish(subject, msg)
+	}
+
+	return nil
+}
+
+type TaskStatus string
+
+const (
+	TaskStatusPending = "Pending"
+	TaskStatusStarted = "Started"
+	TaskStatusRunning = "Running"
+	TaskStatusFailed  = "Failed"
+	TaskStatusSuccess = "Success"
+)
+
+func newResponse(status TaskStatus, id, title, msg string) []byte {
+	m := map[string]string{
+		"status": string(status),
+		"msg":    msg,
+	}
+	if id != "" {
+		m["id"] = id
+	}
+	if title != "" {
+		m["step"] = title
+	}
+	data, _ := json.Marshal(m)
+	return data
 }
